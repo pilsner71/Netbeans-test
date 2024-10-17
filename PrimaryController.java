8fa3d7f420f708c8d9018772eeca524b276f0eea
pilsner71 <jorgen.nilsson@ltu.se>
2024-10-17 10:52:49 +0200

    Add files via upload

---
+++ PrimaryController.java
@@ -0,0 +1,12 @@
+package com.mycompany.mvn2024;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
